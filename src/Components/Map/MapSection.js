723058f8c9ea594cc6cf2798fa82ab0421877c22
kmbear3 <kmbarram@gmail.com>
2022-04-02 15:48:08 -0600

    fix issues with map rendering

--- src/Components/Map/MapSection.js
+++ src/Components/Map/MapSection.js
@@ -8,14 +8,14 @@ export function MapSection(props) {
 
         <MapContainer center={[51.505, -0.09]} zoom={13}>
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-             <Marker position={[51.505, -0.09]}>
-                 <Popup>
-                     A pretty CSS3 popup. <br/> Easily customizable.
-                 </Popup>
-             </Marker>
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
         </MapContainer>
     );
 
